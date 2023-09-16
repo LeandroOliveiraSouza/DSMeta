ff5fa9683feced2bffac990b8011027d17a15fa4
LeandroOliveiraSouza <lendro_oliveirasouza@outlook.com>
2023-09-16 14:45:36 -0300

    UseState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,24 +2,32 @@ import "./styles.css";
 import NotificationButton from "../NotificationButton";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 export default function SalesCard() {
+
+  const start = new Date(new Date().setDate(new Date().getDate() - 365));
+  const end = new Date();
+
+  const [startDate, setStartDate] = useState(start);
+  const [endDate, setEndDate] = useState(end);
+
   return (
     <div className="dsmeta-card">
       <h2 className="dsmeta-sales-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={startDate}
+            onChange={(date: Date) => setStartDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
         </div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={endDate}
+            onChange={(date: Date) => setEndDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
