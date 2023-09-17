c2aea1c80dc8225c8ad771b2d5feff702dfd2e5c
LeandroOliveiraSouza <lendro_oliveirasouza@outlook.com>
2023-09-17 13:37:09 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
 import "./styles.css";
 import notificationIcon from "../../assets/notification.svg";
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
 
-export default function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+    console.log("sucesso");
+  })
+}
+
+export default function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={notificationIcon} alt="Notificar" />
     </div>
   );
